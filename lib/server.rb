36b1555dd1b2f8058ce809e4cd4fc88f849cd5c3
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-06 12:31:51 +0100

    Set up db testing, in process of adding capybara

--- lib/server.rb
+++ lib/server.rb
@@ -1,10 +1,11 @@
 require 'sinatra/base'
+require 'data_mapper'
 
-class BookmarkManager < Sinatra::Base
-  get '/' do
-    'Hello BookmarkManager!'
-  end
+env = ENV['RACK_ENV'] || 'development'
 
-  # start the server if ruby file executed directly
-  run! if app_file == $0
-end
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require_relative 'link'
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
